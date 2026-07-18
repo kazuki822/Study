41b4ed75b61ebe3ca523ae52bb4413b961cec3ab
kazuki822 <tanaka.kazuki20251001@gmail.com>
2026-07-18 18:47:26 +0900

    Ch.1 課題2

---
+++ ch-l2/src/main/java/com/example/demo/ChL2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChL2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChL2Application.class, args);
+	}
+
+}
---
+++ ch-lv2/src/main/java/com/example/demo/ChLv2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChLv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChLv2Application.class, args);
+	}
+
+}
