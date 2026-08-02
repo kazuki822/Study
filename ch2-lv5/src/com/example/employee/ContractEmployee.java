43673ce574532a317c878a4c6f1300b5f3a1adb5
kazuki822 <tanaka.kazuki20251001@gmail.com>
2026-08-02 23:17:21 +0900

    ch2-5

---
+++ ch2-lv5/src/com/example/employee/ContractEmployee.java
@@ -0,0 +1,54 @@
+package com.example.employee;
+
+import java.time.LocalDate;
+
+/**
+ * 契約社員を表すクラス。
+ */
+public class ContractEmployee extends Employee {
+
+    private int monthlyWage;
+    private final int contractMonths;
+
+    public ContractEmployee(String employeeId, String name, String deptId,
+                            int monthlyWage, LocalDate hireDate, int contractMonths) {
+
+        super(
+            employeeId,
+            name,
+            deptId,
+            monthlyWage * 12,
+            hireDate
+        );
+
+        this.monthlyWage = monthlyWage;
+        this.contractMonths = contractMonths;
+    }
+
+    public int getMonthlyWage() {
+        return monthlyWage;
+    }
+
+    public int getContractMonths() {
+        return contractMonths;
+    }
+
+    @Override
+    public int calcMonthlySalary() {
+        return monthlyWage;
+    }
+
+    @Override
+    public String getEmployeeType() {
+        return "契約社員";
+    }
+
+    /**
+     * 年収更新時に月額賃金も更新する
+     */
+    @Override
+    public void updateAnnualSalary(int newSalary) {
+        super.updateAnnualSalary(newSalary);
+        this.monthlyWage = newSalary / 12;
+    }
+}
